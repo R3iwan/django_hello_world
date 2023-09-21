917273e71569575ac19513055a06f3a352b77b88
R3iwan <r3iwan.study@gmail.com>
2023-09-21 15:08:51 +0600

    modified views and urls

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from django.urls import re_path
 from .import views
 
-urlpatterns = {
-    path('', views.index, name='index'),
-}
+urlpatterns = [
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^content', views.content),
+    path('products/<int:product_id>', views.products),
+    path('products/', views.products),
+    path('users/<int:id>/<name>', views.users),
+    path('users/', views.users),
+    path('details/', views.details),
+    path('access/<int:age>', views.access),
+]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.http import HttpResponseBadRequest, HttpResponseForbidden
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World! My First Django Project.")
+    return HttpResponse("<h1>Main Menu</h1>")
+def about(request):
+    return HttpResponse("<h1>About Me</h1>")
+def content(request):
+    return HttpResponseRedirect("/about")
+    #return HttpResponse("<h1>Content</h1>")
+def products(request, product_id=1):
+    category = request.GET.get("cat", "Not Identified")
+    output = "<h2>Product # {0}, Category: {1}</h2>".format(product_id, category)
+    return HttpResponse(output)
+def users(request):
+    id = request.GET.get("id", "Not Identified")
+    name = request.GET.get("name", "Not Identified")
+    output = "<h2>User</h2><h3>id:""{0} Name:{1}</h3>".format(id, name)
+    return HttpResponse(output)
+def details(request):
+    return HttpResponsePermanentRedirect("/")
 
+def access(request, age):
+    if age not in range(1, 111):
+        return HttpResponseBadRequest("Not Correct Data")
+    if age > 17:
+        return HttpResponse("Access permitted")
+    else:
+        return HttpResponseForbidden("Access Denied")
